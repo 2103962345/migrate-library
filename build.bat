ac5f73efe8780ad5838f697879f6512c42415fbb
alexbobylewa <alexbobylewa@gmail.com>
2020-04-30 10:59:28 +0300

    Add bat

--- build.bat
+++ build.bat
@@ -41,7 +41,7 @@ if not "%errorlevel%"=="0" goto failure
 cd tests\Car.Tests
 echo Unit tests
 call "C:\temp\nuget.exe" install xunit.runner.console -Version 2.4.1 -OutputDirectory packages 
-packages\xunit.runner.console.2.4.1\tools\net452\xunit.console.exe /config:%config% /framework:net-4.5 bin\%config%\Car.Tests.dll
+packages\xunit.runner.console.2.4.1\tools\net452\xunit.console.exe bin\%config%\Car.Tests.dll
 
 cd ..\..
 
