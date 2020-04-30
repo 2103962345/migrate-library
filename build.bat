dcbd9aabc51890418a2e53141df5cc9a786adc39
alexbobylewa <alexbobylewa@gmail.com>
2020-04-30 10:13:19 +0300

    Add bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-:@echo Off
+@echo Off
 set config=%1
 if "%config%" == "" (
     set config=Release
@@ -9,10 +9,10 @@ if not "%BuildCounter%" == "" (
    set packversionsuffix=--version-suffix ci-%BuildCounter%
 )
 
-echo Detect MSBuild 15.0 path
+REM Detect MSBuild 15.0 path
 if exist "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\15.0\Bin\MSBuild.exe" (
     set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\15.0\Bin\MSBuild.exe"
-echo %msbuild%
+REM %msbuild%
 )
 if exist "%programfiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
     set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
@@ -32,12 +32,12 @@ call "C:\temp\nuget.exe" restore "src\Car\packages.config" -OutputDirectory %cd%
 call "C:\temp\nuget.exe" restore "tests\Car.Tests\packages.config" -OutputDirectory %cd%\packages -NonInteractive
 :if not "%errorlevel%"=="0" goto failure
 
-echo Build
+REM Build
 call "%msbuild%" Vehicle.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
 cd tests\Car.Tests
-echo Unit tests
+REM Unit tests
 call "C:\temp\nuget.exe" install xunit.runner.console -Version 2.4.1 -OutputDirectory packages\xunit.runner.console.2.4.1\tools\net452\xunit.console.exe /config:%config% /framework:net-4.5 bin\%config%\Car.Tests.dll
 
 cd ..\..
